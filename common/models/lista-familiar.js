08014844d34786a9e569e42d4cfe9e762f4c5084
Rubén <d8vjork@outlook.com>
2017-11-28 10:39:04 +0100

    ListaFamiliar con propietario asignado automaticamente

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Listafamiliar) {
 
+  Listafamiliar.beforeRemote('create', function (ctx, model, next) {
+    if (ctx.req.accessToken) {
+      console.log(ctx.args)
+      ctx.args.data.owner = ctx.req.accessToken.userId
+
+      next()
+    }
+  })
+
 };
--- server/boot/automigrate.js
+++ server/boot/automigrate.js
@@ -3,11 +3,14 @@
 var config = require('../config.development.js')
 
 module.exports = function(app) {
-  //data sources
-  var mysqlDs = app.dataSources.mysqlDs
 
-  mysqlDs.automigrate(null, function (err) {
-    if (err) console.log(err);
-    console.log('> Models migrated to tables');
-  })
+  if (process.env.AUTOMIGRATE) {
+    //data sources
+    var mysqlDs = app.dataSources.mysqlDs
+
+    mysqlDs.automigrate(null, function (err) {
+      if (err) console.log(err)
+      console.log('> Models migrated to tables')
+    })
+  }
 }
